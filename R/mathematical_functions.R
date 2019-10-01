f2d096e628e38deb05da0ad8a0ffea99f5f032ab
Emma Thomas <emmathomas@g.harvard.edu>
2019-10-01 11:27:16 -0700

    Edited description file, added license file, tidied up code formatting

---
+++ R/mathematical_functions.R
@@ -0,0 +1,30 @@
+# --------------------------------------------------------------------------------- #
+# ---------- Useful mathematical functions not implemented in base R  ------------- #
+# --------------------------------------------------------------------------------- #
+
+trace_prod <- function(A, B) {
+  # computes trace(A %*% B) for square matrices A, B
+  sum(t(B) * A)
+}
+
+log1p_exp <- function(x) {
+  # computes log(1 + exp(x))
+  if (x > 20) {
+    return(x)
+  } else {
+    return(log1p(exp(x)))
+  }
+}
+
+log1p.exp.vec <- function(x){
+  # computes log(1 + exp(x)) for x a vector
+  y <- x
+  which.small <- x <= 20
+  y[which.small] <- log1p(exp(x[which.small]))
+  return(y)
+}
+
+loglogit <- function(x) {
+  # computes -log(1+exp(-x)) for x a vector
+  -log1p.exp.vec(-x)
+}
---
+++ R/update_params_normal.R
@@ -0,0 +1,60 @@
+# --------------------------------------------------------------------------------- #
+# --------- Performs one step in VI optimization for Gaussian outcome  ------------ #
+# --------------------------------------------------------------------------------- #
+
+update_params_normal <- function(X, XtX, y, n, K, G, # data
+                                 prob, mu, Sigma, Sigma_inv, Sigma_det, tau_t, # variational params
+                                 sigma2, rho, tau, # hyperparams
+                                 update.hyper=F, update.hyper.last=F ) {
+  # Update tau_t ---------------------------------------------------------------------
+  # only changes if tau has been updated; see below
+  # Update Sigma ---------------------------------------------------------------------
+  if (update.hyper.last) {
+    # Sigma only needs to updated if hyperparams were updated last step
+    Sigma_inv <- plyr::aaply(.data = XtX / sigma2, .margins = 1, 
+                             .fun = function(A, tau, K) A + diag(1 / tau, K), 
+                             tau = tau, K = K, .drop = F)
+    Sigma <- plyr::aaply(.data = Sigma_inv, .margins = 1, .fun = solve, .drop = F)
+    if (K == 1) {
+      Sigma_det <- Sigma[, 1, 1]
+    } else {
+      Sigma_det <- plyr::aaply(.data = Sigma, .margins = 1, .fun = det)
+    }
+  }
+  # Update mu -----------------------------------------------------------------------
+  pred_g <- matrix(0, nrow = n, ncol = G)
+  for (g in 2:G) {
+    pred_g[, g] <- prob[g] * matrix(X[g, , ], nrow = n) %*% matrix(mu[g, ], nrow = K)
+  }
+  for (g in 1:G) {
+    mu[g, ] <- (1 / sigma2) * matrix(Sigma[g, , ], nrow = K) %*% 
+      crossprod(matrix(X[g, , ], nrow = n), y - rowSums(pred_g[, -g, drop = F]))
+    if (g != G) {
+      pred_g[, g] <- prob[g] * matrix(X[g, , ], nrow = n) %*% matrix(mu[g, ], nrow = K)
+    }
+  }
+  # Update prob (pi in manuscript) --------------------------------------------------
+  const <- log(rho / (1 - rho)) - 0.5 * K * log(tau_t)
+  for (g in 1:G) {
+    u <- crossprod(matrix(mu[g, ], nrow = K), matrix(Sigma_inv[g, , ],nrow = K)) %*% 
+      matrix(mu[g, ], nrow = K) + 0.5 * log(Sigma_det[g]) + const
+    prob[g] <- exp(loglogit(u[1, 1]))
+  }
+  # Compute ELBO --------------------------------------------------------------------
+  ELBO <- compute_elbo_normal(X = X, XtX = XtX, y = y, n = n, K = K, G = G,
+                              prob = prob, mu = mu, Sigma = Sigma, Sigma_det = Sigma_det,
+                              tau_t = tau_t, sigma2 = sigma2, rho = rho, tau = tau,
+                              update.hyper = update.hyper)
+  if(update.hyper){
+    sigma2 <- ELBO$sigma2
+    rho <- ELBO$rho
+    tau <- ELBO$tau
+    tau_t <- tau
+    ELBO <- ELBO$ELBO
+  }
+  # Return ---------------------------------------------------------------------------
+  return(list(ELBO = ELBO, prob = prob, mu = mu, Sigma = Sigma, Sigma_inv = Sigma_inv,
+              Sigma_det = Sigma_det, tau_t = tau_t, sigma2 = sigma2, rho = rho, tau = tau))
+  
+}
+
