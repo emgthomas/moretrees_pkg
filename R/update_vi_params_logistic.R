d82abf8137217ddb721fcb1fbd2b1c1747443a74
Emma Thomas <emmathomas@g.harvard.edu>
2020-01-26 14:40:54 -0800

    Coded up logistic spike & slab model; needs debugging

--- R/update_vi_params_logistic.R
+++ R/update_vi_params_logistic.R
@@ -2,35 +2,34 @@
 # --------- Performs one step in VI optimization for Gaussian outcome  ------------ #
 # --------------------------------------------------------------------------------- #
 
-update_vi_params_normal <- function(X, XtX, W, WtW, y, n, K, G, m, # data
-                                 prob, mu, Sigma, Sigma_inv, Sigma_det, tau_t, 
-                                 delta, Omega, Omega_inv, Omega_det, # variational params
-                                 omega, sigma2, rho, tau, # hyperparams
-                                 update_hyper_last) { 
+update_vi_params_logistic <- function(X, W, y, n, K, G, m, # data
+                                    prob, mu, Sigma, Sigma_inv, Sigma_det, tau_t, 
+                                    delta, Omega, Omega_inv, Omega_det, 
+                                    eta, g_eta, # variational params
+                                    omega, rho, tau) { # hyperparams
   # Update sparse coefficients ------------------------------------------------------
   Wdelta <- W %*% delta
+  A_eta <- Matrix::Diagonal(n = n, g_eta)
   pred_g <- matrix(0, nrow = n, ncol = G)
   for (g in 2:G) {
     pred_g[, g] <- prob[g] * matrix(X[g, , ], nrow = n) %*% matrix(mu[g, ], nrow = K)
   }
+  # Update Sigma_g and tau_t_g
   for (g in 1:G) {
-    # Update Sigma_g and tau_t_g only if hyperparameters were updated last round
-    if (update_hyper_last) {
-      Sigma_inv[g, , ] <- XtX[g, , ] / sigma2 + diag(1 / tau, K)
-      Sigma[g, , ] <- solve(Sigma_inv[g, , ])
-      if (K == 1) {
-        Sigma_det[g] <- Sigma[g, 1, 1]
-      } else {
-        Sigma_det[g] <- solve(Sigma[g, , ])
-      }
-      tau_t[g] <- tau
+    Sigma_inv[[g]] <- 2 * t(X[g, , ]) %*% A_eta %*% X[g, , ] + diag(1 / tau, K)
+    Sigma[[g]] <- solve(Sigma_inv[[g]])
+    if (K == 1) {
+      Sigma_det[g] <- Sigma[[g]]
+    } else {
+      Sigma_det[g] <- det(Sigma[[g]])
     }
+    tau_t[g] <- tau
     # update mu_g
-    mu[g, ] <- (1 / sigma2) * matrix(Sigma[g, , ], nrow = K) %*%
+    mu[g, ] <- Sigma[[g]] %*%
       crossprod(matrix(X[g, , ], nrow = n),
-                y - Wdelta - rowSums(pred_g[, -g, drop = F]))
+                y / 2 - 2 * g_eta * (Wdelta + rowSums(pred_g[, -g, drop = F])))
     # update prob_g (pi_g in manuscript)
-    u <- crossprod(matrix(mu[g, ], nrow = K), matrix(Sigma_inv[g, , ],nrow = K)) %*%
+    u <- crossprod(matrix(mu[g, ], nrow = K), matrix(Sigma_inv[[g]],nrow = K)) %*%
       matrix(mu[g, ], nrow = K) + 0.5 * log(Sigma_det[g]) + 
       log(rho / (1 - rho)) - 0.5 * K * log(tau_t[g])
     prob[g] <- expit(as.numeric(u))
@@ -39,17 +38,15 @@ update_vi_params_normal <- function(X, XtX, W, WtW, y, n, K, G, m, # data
   }
   # Update non-sparse coefficients ---------------------------------------------------
   # Update Omega only if hyperparameters were updated at last step
-  if (update_hyper_last) {
-    Omega_inv <- WtW / sigma2 + diag(1 / omega, m)
-    Omega <- solve(Omega_inv)
-    if (m == 1) {
-      Omega_det <- Omega[1, 1]
-    } else {
-      Omega_det <- det(Omega)
-    }
+  Omega_inv <- 2 * t(W) %*% A_eta %*% W + diag(1 / omega, m)
+  Omega <- solve(Omega_inv)
+  if (m == 1) {
+    Omega_det <- Omega[1, 1]
+  } else {
+    Omega_det <- det(Omega)
   }
   # Update delta
-  delta <- (1 / sigma2) * Omega %*% t(W) %*% (y - rowSums(pred_g))
+  delta <- as.numeric(Omega %*% t(W) %*% (y / 2 - 2 * g_eta * rowSums(pred_g)))
   # Return ---------------------------------------------------------------------------
   return(list(prob = prob, mu = mu, Sigma = Sigma, Sigma_inv = Sigma_inv,
               Sigma_det = Sigma_det, tau_t = tau_t, delta = delta,
---
+++ tests/logistic_ss_test.R
@@ -0,0 +1,89 @@
+# --------------------------------------------------------------------------------- #
+# -------- Spike & slab group variable selection for Gaussian outcome  ------------ #
+# -------- Test code -------------------------------------------------------------- #
+# --------------------------------------------------------------------------------- #
+
+set.seed(98647)
+devtools::load_all() # Sources all files in R/
+# Input parameters ------------------------------------------------------------------
+K <- 2
+G <- 10 # note: for matrices/arrays indexed by g=1,...,G, g is always the first dimension
+m <- 2
+# beta <- matrix(sample(c(-1, 1), replace = T, size = G * K,
+#                       prob = c(0.25, 0.75)), nrow = G)
+tau <- 3
+rho <- 0.3
+omega <- 2
+gamma_true <- matrix(rnorm(G * K, mean = 0, sd = sqrt(tau)), nrow = G)
+s_true <- rbinom(n = G, size = 1, prob = rho)
+beta <- gamma_true * s_true
+theta <- rnorm(m, mean = 0, sd = sqrt(omega))
+n <- 300
+# Generate some data -----------------------------------------------------------------
+X <- array(rnorm(K * G * n, sd = 0.5), dim = c(G, n, K))
+W <- matrix(rnorm(m * n, sd = 0.5), nrow = n)
+lp <- W %*% theta
+for (g in 1:G) {
+  lp <- lp + matrix(X[g, , ], nrow = n) %*% matrix(beta[g, ], nrow = K)
+}
+lp <- as.numeric(lp)
+expit <- 1 / (1 + exp(-lp))
+y <- sapply(expit, rbinom, n = 1, size = 1)
+# Run algorithm ----------------------------------------------------------------------
+mod1 <- spike_and_slab_logistic(y, X, W, update_hyper = T, update_hyper_freq = 10,
+                              tol = 1E-8, max_iter = 100)
+                              # hyperparams_init = list(omega = omega,
+                              #                         rho = 0.05,
+                              #                         tau = tau,
+                              #                         sigma2 = sigma2))
+
+# Plot results -----------------------------------------------------------------------
+
+# Check if the ELBO decreases
+ELBO_track <- mod1$ELBO_track2
+if(min(ELBO_track[2:length(ELBO_track)] - ELBO_track[1:(length(ELBO_track)-1)]) < 0) {
+  print("ELBO decreases at these time points:")
+  which(ELBO_track[2:length(ELBO_track)] - ELBO_track[1:(length(ELBO_track)-1)] < 0)
+} else {
+  print("ELBO always increases!")
+}
+
+# ELBO at every time step
+plot_start <- 1
+plot_end <- length(ELBO_track)
+# plot_end <- 240
+plot(plot_start:plot_end,
+     ELBO_track[plot_start:plot_end],
+     type = "l")
+
+# Compare sparse effect estimates to truth --------------------------------------------------
+# compare estimated probabilities of variable inclusion to true variable inclusion indicators
+tapply(mod1$vi_params$prob, s_true, summary) 
+plot(mod1$vi_params$prob, s_true)
+# compare estimated coefficients to true coefficients
+plot(mod1$vi_params$mu * mod1$vi_params$prob, beta)
+abline(a = 0, b = 1, col = "red")
+
+# Compare non-sparse effect estiamtes to truth ----------------------------------------------
+plot(mod1$vi_params$delta, theta)
+abline(a = 0, b = 1, col = "red")
+
+# Compare hyperparameter estimates to truth -------------------------------------------------
+
+cbind(mod1$hyperparams[2:4], c(omega, tau, rho))
+
+# ELBO when hyperparams updated
+plot_start <- 1
+plot(plot_start:length(mod1$ELBO_track),
+     mod1$ELBO_track[plot_start:length(mod1$ELBO_track)],
+     type = "l")
+plot(plot_start:length(mod1$rho_track),
+     mod1$rho_track[plot_start:length(mod1$rho_track)],
+     type = "l")
+plot(plot_start:length(mod1$tau_track),
+     mod1$tau_track[plot_start:length(mod1$tau_track)],
+     type = "l")
+plot(plot_start:length(mod1$sigma2_track),
+     mod1$sigma2_track[plot_start:length(mod1$sigma2_track)],
+     type = "l")
+
