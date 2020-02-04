b1632bd01c0015afb812fec6d7a167e1ece9cd53
Emma Thomas <emmathomas@g.harvard.edu>
2020-02-03 16:02:35 -0800

    Example where ELBO decreases slightly

--- tests/gaussian_ss_test.R
+++ tests/gaussian_ss_test.R
@@ -3,22 +3,22 @@
 # -------- Test code -------------------------------------------------------------- #
 # --------------------------------------------------------------------------------- #
 
-seed <- sample(1:1e6, size = 1)
-# seed <- 180361
+# seed <- sample(1:1e6, size = 1)
+seed <- 524586
 set.seed(seed)
 devtools::load_all() # Sources all files in R/
 # Input parameters ------------------------------------------------------------------
-K <- 2
-G <- 100 # note: for matrices/arrays indexed by g=1,...,G, g is always the first dimension
+K <- 4
+G <- 200 # note: for matrices/arrays indexed by g=1,...,G, g is always the first dimension
 # beta <- matrix(sample(c(-1, 1), replace = T, size = G * K,
 #                       prob = c(0.25, 0.75)), nrow = G)
-tau <- 3
-rho <- 0.3
+tau <- 4
+rho <- 0.2
 gamma_true <- matrix(rnorm(G * K, mean = 0, sd = sqrt(tau)), nrow = G)
 s_true <- rbinom(n = G, size = 1, prob = rho)
 beta <- gamma_true * s_true
-sigma2 <- 3
-n <- 1000
+sigma2 <- 4
+n <- 2000
 # Generate some data -----------------------------------------------------------------
 X <- array(rnorm(K * G * n), dim = c(G, n, K))
 lp <- numeric(n) + 0
@@ -28,11 +28,11 @@ for (g in 1:G) {
 lp <- as.numeric(lp)
 y <- lp + rnorm(n, mean = 0, sd = sqrt(sigma2))
 # Run algorithm ----------------------------------------------------------------------
-mod1 <- spike_and_slab_normal(y, X, update_hyper = T, update_hyper_freq = 10,
-                              tol = 1E-8,
-                              hyperparams_init = list(rho = rho,
-                                                      tau = tau,
-                                                      sigma2 = sigma2))
+mod1 <- spike_and_slab_normal(y, X, update_hyper = T, update_hyper_freq = 50,
+                              tol = 1E-8)
+                              # hyperparams_init = list(rho = rho,
+                              #                         tau = tau,
+                              #                         sigma2 = sigma2))
 
 # Plot results -----------------------------------------------------------------------
 
@@ -46,9 +46,9 @@ if(min(ELBO_track[2:length(ELBO_track)] - ELBO_track[1:(length(ELBO_track)-1)]) 
 }
 
 # ELBO at every time step
-plot_start <- 4
+plot_start <- 1
 plot_end <- length(ELBO_track)
-# plot_end <- 16
+# plot_end <- 2604
 plot(plot_start:plot_end,
      ELBO_track[plot_start:plot_end],
      type = "l")
