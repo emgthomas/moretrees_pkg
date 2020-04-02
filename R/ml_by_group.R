0a240850ecda4ec26503016453ccbd74addee16e
Emma Thomas <emmathomas@g.harvard.edu>
2020-04-02 11:29:27 -0700

    Added option to return theta to ml_by_group

--- R/ml_by_group.R
+++ R/ml_by_group.R
@@ -27,23 +27,38 @@
 #' either "bernoulli" (for classification) or "gaussian" (for regression)
 #' @param ci_level A number between 0 and 1 giving the desired credible interval.
 #' For example, ci_level = 0.95 (the default) returns a 95% credible interval.
+#' @param return_theta Return ML estimates for theta? Default = FALSE
 #' @return A list containing the following elements:
 #' 1. estimated coefficients and credible intervals; 
 #' 2. outputs from variational inference algorithm
 #' @examples
 #' @family spike and slab functions
 
-ml_by_group <- function(X, W = NULL, y, outcomes, outcome_groups, ci_level, family) {
+ml_by_group <- function(X, W = NULL, y, outcomes, outcome_groups,
+                        ci_level, family, return_theta = FALSE) {
   G <- length(outcome_groups)
   K <- ncol(X)
   beta_ml <- matrix(nrow = G, ncol = K * 3 + 1) %>%
     as.data.frame
-  cols <- c("est", "cil", "ciu")
-  cols <- sapply(1:K, function(i) paste0(cols, i), simplify = T) %>%
+  cols <- sapply(1:K, 
+    function(i) paste0(c("est", "cil", "ciu"), i), simplify = T) %>%
     as.vector
   names(beta_ml) <- c("group", cols)
   beta_ml$group <- 1:G
   beta_ml$outcomes <- outcome_groups
+  if (!is.null(W)) {
+    m <- ncol(W)
+    if (return_theta) {
+      theta_ml <- matrix(nrow = G, ncol = m * 3 + 1) %>%
+        as.data.frame
+    }
+    cols <- sapply(1:m, 
+      function(i) paste0(c("est", "cil", "ciu"), i), simplify = T) %>%
+      as.vector
+    names(theta_ml) <- c("group", cols)
+    theta_ml$group <- 1:G
+    theta_ml$outcomes <- outcome_groups
+  }
   for (g in 1:G) {
     which_i <- outcomes %in% outcome_groups[[g]]
     if (!is.null(W)) {
@@ -59,7 +74,15 @@ ml_by_group <- function(X, W = NULL, y, outcomes, outcome_groups, ci_level, fami
     beta_ml[g, paste0("est", 1:K)] <- mod_ml$coefficients[1:K]
     beta_ml[g, paste0("cil", 1:K)] <- beta_ml_ci[1:K , 1]
     beta_ml[g, paste0("ciu", 1:K)] <- beta_ml_ci[1:K , 2]
+    if (return_theta) {
+      theta_ml[g, paste0("est", 1:m)] <- mod_ml$coefficients[(K + 1):(K + m)]
+      theta_ml[g, paste0("cil", 1:m)] <- beta_ml_ci[(K + 1):(K + m) , 1]
+      theta_ml[g, paste0("ciu", 1:m)] <- beta_ml_ci[(K + 1):(K + m) , 2]
+    }
   }
-  
-  return(beta_ml)
+  if (return_theta) {
+    return(list(beta_ml = beta_ml, theta_ml = theta_ml))
+  } else {
+    return(beta_ml)
+  }
 }
