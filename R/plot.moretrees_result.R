7db8d91d83c25b4121f8d78f1d4d3514824be1be
Emma Thomas <emmathomas@g.harvard.edu>
2020-04-12 18:36:16 -0700

    Changed arguments in plot.moretrees_result for compatibility with generic plot method

--- R/plot.moretrees_result.R
+++ R/plot.moretrees_result.R
@@ -4,7 +4,7 @@
 #' 
 #' @export
 #' @importFrom ggtree %<+%
-#' @param res Output from \code{moretrees()}
+#' @param x Output from \code{moretrees()}
 #' @param group.text.size Text size for the group labels
 #' @param group.text.offset Offset of the group label from the 
 #' leaves of the tree
@@ -16,6 +16,7 @@
 #' the root node at the top and all other nodes below. If FALSE, the tree
 #' will be plotted with the root node to the left and all other nodes to
 #' the right.
+#' @param ... Not used.
 #' @return A plot showing the groups discovered by MOReTreeS on the original
 #' outcome tree.
 #' @examples 
@@ -23,20 +24,21 @@
 #' vignette("moretrees")
 #' @family Tree functions
 
-plot.moretrees_result <- function(res,
+plot.moretrees_result <- function(x,
                                   group.text.size = 4,
                                   group.text.offset = 0.1,
                                   legend.text.size = 10,
                                   layout = "rectangular",
-                                  horizontal = TRUE) {
+                                  horizontal = TRUE,
+                                  ...) {
   
   # Get data.frame with groups info
-  leaves <- names(igraph::V(res$tr)[igraph::degree(res$tr, mode = "out") == 0])
-  groups.df <- data.frame(leaves = leaves, Group = as.factor(res$beta_est$group))
+  leaves <- names(igraph::V(x$tr)[igraph::degree(x$tr, mode = "out") == 0])
+  groups.df <- data.frame(leaves = leaves, Group = as.factor(x$beta_est$group))
   cols_g <- RColorBrewer::brewer.pal(length(levels(groups.df$Group)), "Set3")
   
   # Plot
-  p <- ggtree::ggtree(res$tr, ladderize = F, layout = layout)  %<+% groups.df
+  p <- ggtree::ggtree(x$tr, ladderize = F, layout = layout)  %<+% groups.df
   if (horizontal) {
     p <- p + ggplot2::coord_flip() + ggplot2::scale_x_reverse()
     group.text.offset <- - group.text.offset
